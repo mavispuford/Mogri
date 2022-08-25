ddf795662e3847afd58a0c72dd8ea3d6fb5d10aa
mavispuford <mavispuford@gmail.com>
2022-08-25 17:05:05 -0600

    Initial commit, basic page layout, architecture, and file writing for Android

---
+++ MobileDiffusion/Interfaces/Services/IFileService.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileDiffusion.Interfaces.Services
+{
+    public interface IFileService
+    {
+        Task WriteFileToExternalStorageAsync(string fileName, Stream stream);
+
+        Stream GetFileStreamFromExternalStorage(string fileName);
+    }
+}
---
+++ MobileDiffusion/ViewRegistrations.cs
@@ -0,0 +1,42 @@
+﻿using MobileDiffusion.Interfaces.ViewModels;
+using MobileDiffusion.Views;
+using System.ComponentModel;
+
+namespace MobileDiffusion
+{
+    public static class ViewRegistrations
+    {
+        public static MauiAppBuilder RegisterViews(this MauiAppBuilder builder)
+        {
+            registerPage<IMainPageViewModel, MainPage>(builder.Services);
+
+            return builder;
+        }
+
+        private static void registerPage<TViewModel, TPage>(IServiceCollection serviceCollection)
+            where TViewModel : IBaseViewModel
+            where TPage : Page
+        {
+            serviceCollection.AddTransient(provider =>
+            {
+                var page = Activator.CreateInstance(typeof(TPage)) as TPage;
+
+                if (page == null)
+                {
+                    throw new InvalidOperationException($"Unable to create a page of type {typeof(TPage)}");
+                }
+
+                var viewModel = provider.GetService<TViewModel>();
+
+                if (viewModel == null)
+                {
+                    throw new InvalidOperationException($"Unable to create a viewmodel of type {typeof(TViewModel)}");
+                }
+
+                page.BindingContext = viewModel;
+
+                return page;
+            });
+        }
+    }
+}
