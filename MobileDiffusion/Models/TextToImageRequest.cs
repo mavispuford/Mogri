4a2cc4f052e5812cc94ffcc56902f346eb5285d7
mavispuford <mavispuford@gmail.com>
2022-08-28 16:35:59 -0600

    Added IStableDiffusionService and TextToImageRequest

---
+++ MobileDiffusion/Models/TextToImageRequest.cs
@@ -0,0 +1,39 @@
+﻿namespace MobileDiffusion.Models;
+
+public class TextToImageRequest
+{
+    /// <summary>
+    ///     The text prompt.
+    /// </summary>
+    public string Prompt { get; set; }
+
+    /// <summary>
+    ///     The seed.
+    /// </summary>
+    public long Seed { get; set; }
+
+    /// <summary>
+    ///     The number of outputs.
+    /// </summary>
+    public short NumberOfOutputs { get; set; }
+    
+    /// <summary>
+    ///     The width (in pixels).
+    /// </summary>
+    public short Width { get; set; }
+
+    /// <summary>
+    ///     The height (in pixels).
+    /// </summary>
+    public short Height { get; set; }
+
+    /// <summary>
+    ///     The number of steps.
+    /// </summary>
+    public short Steps { get; set; }
+
+    /// <summary>
+    ///     The guidance scale (aka CFG_Scale).
+    /// </summary>
+    public float GuidanceScale { get; set; }
+}
