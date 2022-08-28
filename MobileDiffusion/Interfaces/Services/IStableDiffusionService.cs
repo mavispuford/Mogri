026f2c6bcf79b2966d58b4509c61c773446cf8ed
mavispuford <mavispuford@gmail.com>
2022-08-28 16:35:59 -0600

    Added IStableDiffusionService and TextToImageRequest

---
+++ MobileDiffusion/Interfaces/Services/IStableDiffusionService.cs
@@ -0,0 +1,8 @@
+﻿using MobileDiffusion.Models;
+
+namespace MobileDiffusion.Interfaces.Services;
+
+public interface IStableDiffusionService
+{
+    public Task SubmitTextToImageRequest(TextToImageRequest request);
+}
