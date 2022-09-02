198451a0ddba769711933bc86211300a637f3070
mavispuford <mavispuford@gmail.com>
2022-09-02 15:47:08 -0600

    Simple prompt settings layout and VM properties

--- MobileDiffusion/Interfaces/ViewModels/IPromptSettingsPageViewModel.cs
+++ MobileDiffusion/Interfaces/ViewModels/IPromptSettingsPageViewModel.cs
@@ -2,4 +2,23 @@
 
 public interface IPromptSettingsPageViewModel : IBaseViewModel
 {
+    List<string> AvailableWidthValues { get; set; }
+
+    List<string> AvailableHeightValues { get; set; }
+
+    List<string> AvailableSamplerValues { get; set; }
+
+    string ImageCount { get; set; }
+
+    string Steps { get; set; }
+
+    string CfgScale { get; set; }
+
+    string Sampler { get; set; }
+
+    string Width { get; set; }
+    
+    string Height { get; set; }
+
+    string Seed { get; set; }
 }
