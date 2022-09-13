db4e40f1fb2db94f10566ef992450f1431427f1c
mavispuford <mavispuford@gmail.com>
2022-09-12 18:16:43 -0600

    Added a progress bar at the top of main page

--- MobileDiffusion/Interfaces/ViewModels/IMainPageViewModel.cs
+++ MobileDiffusion/Interfaces/ViewModels/IMainPageViewModel.cs
@@ -6,8 +6,10 @@ namespace MobileDiffusion.Interfaces.ViewModels;
 public interface IMainPageViewModel : IPageViewModel
 {
     bool HasInitImage { get; set; }
+    bool InProgress { get; set; }
     string Prompt { get; set; }
     string PlaceholderPrompt { get; set; }
+    float Progress { get; set; }
     ObservableCollection<IResultItemViewModel> Results { get; set; }
     IAsyncRelayCommand CreateCommand { get; }
     IAsyncRelayCommand ShowImageToImageSettingsCommand { get; }
