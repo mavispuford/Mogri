278cd2f158a6c1c4213c10014b8319b965df2c4d
mavispuford <mavispuford@gmail.com>
2022-09-21 00:22:13 -0600

    Mask page design changes, added brush size and alpha sliders, alpha now controls the color interpolation strength (but not the final alpha of the masked pixels - that is still near-zero)

--- MobileDiffusion/Models/MaskLine.cs
+++ MobileDiffusion/Models/MaskLine.cs
@@ -4,6 +4,8 @@ namespace MobileDiffusion.Models;
 
 public class MaskLine
 {
+    public float Alpha { get; set; }
+    public float BrushSize { get; set; }
     public Color Color { get; set; }
     public List<SKPoint> Path { get; set; } = new();
 }
