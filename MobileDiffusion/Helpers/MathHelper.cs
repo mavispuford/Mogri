bf14d0e3fa592d856ea3cf4d1ba6ed3a54d83c67
mavispuford <mavispuford@gmail.com>
2022-09-30 11:33:57 -0600

    Added random noise to pixels when generating masks, some comments

---
+++ MobileDiffusion/Helpers/MathHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace MobileDiffusion.Helpers;
+
+public static class MathHelper
+{
+    /// <summary>
+    ///     Clamps an integer value between the min and max.
+    /// </summary>
+    /// <param name="value">The input value.</param>
+    /// <param name="min">The minimum.</param>
+    /// <param name="max">The maximum.</param>
+    /// <returns>The clamped integer value.</returns>
+    public static int Clamp(int value, int min, int max)
+    {
+        return Math.Max(Math.Min(value, max), min);
+    }
+}
