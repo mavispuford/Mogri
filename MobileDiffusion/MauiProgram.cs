21467ee84dfd0073c35d8031c61d236acbb59ce8
mavispuford <mavispuford@gmail.com>
2023-07-03 09:50:54 -0600

    Made changes to toolbar items on Canvas page (they are now ImageButtons which allows for tooltips), ripped out workaround for Image crashing bug (finally!)

--- MobileDiffusion/MauiProgram.cs
+++ MobileDiffusion/MauiProgram.cs
@@ -1,8 +1,8 @@
 ﻿using CommunityToolkit.Maui;
 using SkiaSharp.Views.Maui.Controls.Hosting;
 using MobileDiffusion.Registrations;
-using Microsoft.Maui.Platform;
 using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
 
 namespace MobileDiffusion;
 
@@ -43,13 +43,6 @@ public static class MauiProgram
         PickerHandler.Mapper.AppendToMapping("NoUnderline", (h, v) => 
 		{
             h.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
-        });
-
-        // Workaround for bug:
-        // https://github.com/dotnet/maui/issues/9011#issuecomment-1333534818
-        ImageHandler.Mapper.PrependToMapping("Fix Bug 9011", (h, v) =>
-		{
-            h.PlatformView?.Clear();
         });
 #endif
 
