d875ac46f1faf128a87fc4b5651832e2b669e1d5
Blake Ross <mavispuford@gmail.com>
2025-12-20 11:51:17 -0700

    Converted more private field ObservableProperties

--- MobileDiffusion/ViewModels/CanvasActions/MaskLineViewModel.cs
+++ MobileDiffusion/ViewModels/CanvasActions/MaskLineViewModel.cs
@@ -12,13 +12,13 @@ public partial class MaskLineViewModel : CanvasActionViewModel
     private SKColor _paintColor;
 
     [ObservableProperty]
-    private float _alpha;
+    public partial float Alpha { get; set; }
 
     [ObservableProperty]
-    private float _brushSize;
+    public partial float BrushSize { get; set; }
     
     [ObservableProperty]
-    private Color _color;
+    public partial Color Color { get; set; }
 
     public List<SKPoint> Path { get; set; } = new();
     public MaskEffect MaskEffect { get; set; }
