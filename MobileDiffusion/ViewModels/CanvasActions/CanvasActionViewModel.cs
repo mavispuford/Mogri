91afb461a7637c8a52c283fdea407b1746597606
Blake Ross <mavispuford@gmail.com>
2026-01-17 14:15:50 -0700

    Add ability to save segmentation masks as well as mask lines, reapply mask canvas actions in order that they were originally applied

--- MobileDiffusion/ViewModels/CanvasActions/CanvasActionViewModel.cs
+++ MobileDiffusion/ViewModels/CanvasActions/CanvasActionViewModel.cs
@@ -7,5 +7,7 @@ public abstract class CanvasActionViewModel : BaseViewModel
 {
     public CanvasActionType CanvasActionType { get; set; }
 
+    public int Order { get; set; }
+
     public abstract void Execute(SKCanvas canvas, SKImageInfo imageInfo, bool isSaving);
 }
