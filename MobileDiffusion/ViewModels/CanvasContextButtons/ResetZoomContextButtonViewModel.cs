51abcfe4b5e1d3c34d4fe685a4d8f6e8e5c85849
Blake Ross <mavispuford@gmail.com>
2026-01-17 12:19:22 -0700

    Add Reset Zoom context button and associated ViewModel

---
+++ MobileDiffusion/ViewModels/CanvasContextButtons/ResetZoomContextButtonViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MobileDiffusion.Interfaces.ViewModels;
+
+namespace MobileDiffusion.ViewModels.CanvasContextButtons;
+
+public partial class ResetZoomContextButtonViewModel : CanvasContextButtonViewModel
+{
+    public ResetZoomContextButtonViewModel(ICanvasPageViewModel parentPage) : base(parentPage)
+    {
+    }
+}
